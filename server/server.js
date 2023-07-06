73660a88e7a5b3ef771c93c6cdef3f9b84937a0f
avcopan <avcopan@gmail.com>
2023-07-05 20:26:59 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const plantRouter = require('./routes/plant-router.js');
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// Body parser middleware
+app.use(express.static('build'));
+app.use(express.json());
+
+/* Routes */
+app.use('/api/plant', plantRouter);
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
