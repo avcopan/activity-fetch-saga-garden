fc51461d55e63df223af13ce7ee3e11b427886a2
Andreas Copan <avcopan@gmail.com>
2023-07-05 22:00:22 -0400

    WIP -- not functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import Garden from './components/Garden/Garden';
+import Garden from "./components/Garden/Garden";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const plants = useSelector(store => store.plantList);
 
   return (
     <div className="app">
       <header className="app-header">
         <h1>Welcome to your garden!</h1>
       </header>
-      <Garden />
+      <Router>
+        <Route path="/">
+          <Garden />
+        </Route>
+        {plants.map((plant) => {
+          return(
+            <Route path={`/path/${plant.id}`}>
+            </Route>
+          )
+        })}
+      </Router>
     </div>
   );
 }
